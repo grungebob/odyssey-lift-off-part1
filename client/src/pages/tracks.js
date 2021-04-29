920c2e1710f71d55ae26379e2a3b47de920e7ff4
Robert Chung <chungrob@gmail.com>
2021-04-29 13:57:47 -0400

    Apollo Client Setup

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Layout } from '../components';
+import { gql } from '@apollo/client';
 
+const TRACKS = gql``;
 /**
  * Tacks Page is Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
